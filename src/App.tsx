d1b462530f2626b05ac4bc27ef09dd012abfe8ed
Revolution000001 <pvttys@mail.ru>
2023-08-04 12:20:57 +0500

    react query

--- src/App.tsx
+++ src/App.tsx
@@ -4,56 +4,111 @@ import axios from "axios";
 import {IProduct} from "./models";
 import Products from "./Products";
 import Pagination from "./Pagination";
+import {useMutation, useQuery, useQueryClient} from "react-query";
+
+async function fetchProducts() {
+    const { data } = await axios.get<IProduct[]>('https://fakestoreapi.com/products')
+    return data
+}
+
+async function createProduct(body: IProduct) {
+    return await axios.post<IProduct>('https://fakestoreapi.com/products', body)
+}
 
 function App() {
-    const [products, setProducts] = useState<IProduct[]>([])
-    const [loading, setLoading] = useState(false)
-    const [currentPage, setCurrentPage] = useState<number>(1)
-    const [productsPerPage] = useState<number>(5)
+
+    const queryClient = useQueryClient()
+
+    // const [products, setProducts] = useState<IProduct[]>([])
+    // const [loading, setLoading] = useState(false)
+    // const [currentPage, setCurrentPage] = useState<number>(1)
+    // const [productsPerPage] = useState<number>(5)
     const [searchTerm, setSearchTerm] = useState<string>("")
 
-    useEffect(() => {
-        const getProducts = async () => {
-            setLoading(true)
-            const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products')
-            setProducts(response.data)
-            setLoading(false)
-        }
-        getProducts()
-    }, [])
+    const {data, isError, isLoading, isFetching} = useQuery<IProduct[]>(
+        'products',
+        fetchProducts,
+        {
+            refetchOnWindowFocus: false
+        })
 
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
-    const nextPage = () => setCurrentPage(prev => prev + 1)
-    const prevPage = () => setCurrentPage(prev => prev - 1)
+    const mutation = useMutation(
+        (newProduct: IProduct) => createProduct(newProduct),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(["products"])
+            }
+        }
+    )
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        const body: IProduct = {
+            title: 'test product',
+            price: 13.5,
+            description: 'lorem ipsum set',
+            image: 'https://i.pravatar.cc',
+            category: 'electronic'
+        }
+
+        mutation.mutate(body)
+    }
+
+    if (isLoading) return <h2>Loading</h2>
+
+    if(isError) return <h2>Error</h2>
+
+    if(!data) return <h2>Error data</h2>
+    // console.log(result)
+
+    // useEffect(() => {
+    //     const getProducts = async () => {
+    //         setLoading(true)
+    //         const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products')
+    //         setProducts(response.data)
+    //         setLoading(false)
+    //     }
+    //     getProducts()
+    // }, [])
+
+    // const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+    // const nextPage = () => setCurrentPage(prev => prev + 1)
+    // const prevPage = () => setCurrentPage(prev => prev - 1)
 
     const handleSearch = (e: { target: { value: string}}) => {
-        setCurrentPage(1)
+        // setCurrentPage(1)
         setSearchTerm(e.target.value)
     }
 
-    const bySearch = (product: IProduct, search: string) => {
-        if (search) {
-            return product.title.toLowerCase().includes(search.toLowerCase());
-        } else return product;
-    };
+    // const bySearch = (product: IProduct, search: string) => {
+    //     if (search) {
+    //         return product.title.toLowerCase().includes(search.toLowerCase());
+    //     } else return product;
+    // };
+    //
+    // const filteredList = (products: IProduct[], search: string) => {
+    //     return products.filter(product => bySearch(product, search))
+    // }
 
-    const filteredList = (products: IProduct[], search: string) => {
-        return products.filter(product => bySearch(product, search))
-    }
+    // const indexLastPage = currentPage * productsPerPage
+    // const indexFirstPage = indexLastPage - productsPerPage
+    // const currentProduct = filteredList(products, searchTerm).slice(indexFirstPage, indexLastPage)
 
-    const indexLastPage = currentPage * productsPerPage
-    const indexFirstPage = indexLastPage - productsPerPage
-    const currentProduct = filteredList(products, searchTerm).slice(indexFirstPage, indexLastPage)
+
 
     return (
         <div className="container">
             <h1>Products</h1>
             <input placeholder="Search..." className="form-control mb-2" type="text" name="search" onChange={handleSearch}/>
-            <Products productsPerPage={productsPerPage} currentPage={currentPage} products={currentProduct} loading={loading}/>
-            <Pagination currentPage={currentPage} paginate={paginate} totalProducts={filteredList(products, searchTerm).length} productPerPage={productsPerPage}/>
+            <form onSubmit={onSubmit} action="">
+                <button>Добавить</button>
+            </form>
+            <Products products={data} searchTerm={searchTerm} />
+            {/*<Products productsPerPage={productsPerPage} currentPage={currentPage} products={currentProduct} loading={loading}/>*/}
+            {/*<Pagination currentPage={currentPage} paginate={paginate} totalProducts={filteredList(products, searchTerm).length} productPerPage={productsPerPage}/>*/}
 
-            <button className="btn btn-primary" onClick={prevPage}>Prev page</button>
-            <button className="btn btn-primary ml-2" onClick={nextPage}>Next page</button>
+            {/*<button className="btn btn-primary" onClick={prevPage}>Prev page</button>*/}
+            {/*<button className="btn btn-primary ml-2" onClick={nextPage}>Next page</button>*/}
         </div>
     );
 }
--- src/Products.tsx
+++ src/Products.tsx
@@ -1,23 +1,46 @@
 import {IProduct} from "./models";
+import {useEffect, useState} from "react";
 
 interface Products {
     products: IProduct[]
-    loading: boolean
-    productsPerPage: number
-    currentPage: number
+    // loading: boolean
+    searchTerm: string
+    // productsPerPage: number
+    // currentPage: number
 }
 
-const Products = ({products, loading, productsPerPage, currentPage}: Products) => {
+const Products = (
+    {
+        products,
+        // loading,
+        searchTerm
+        // productsPerPage,
+        // currentPage
+    }: Products
+) => {
 
-    if (loading) {
-        return <h2>Loading</h2>
-    }
+    const [filterProducts, setFilterProducts] = useState<IProduct[]>([])
+
+    useEffect(() => {
+        setFilterProducts(products)
+    }, [products])
+
+    useEffect(() => {
+        const filterProducts = (products: IProduct[]) => {
+            if(searchTerm)
+                setFilterProducts(products.filter((product) => product.title.toLowerCase().includes(searchTerm.toLowerCase())))
+        }
+        filterProducts(products)
+    }, [searchTerm])
+
+
 
     return (
         <ul className="list-group">
-            {products.map((product, i) => (
+            {filterProducts.map((product, i) => (
                 <li key={i} className="list-group-item">
-                    {(currentPage - 1) * productsPerPage + i + 1}. {product.title}
+                    {/*{(currentPage - 1) * productsPerPage + i + 1}. {product.title}*/}
+                    {product.title}
                     <img src={product.image} className="ml-2" style={{width: 25}}/>
                 </li>
             ))}
